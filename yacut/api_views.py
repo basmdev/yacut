804925bcd7e8ecbe10a108fb02da31836c47a37d
Мохьмад Басханов <basm@upr.local>
2023-11-14 10:25:56 +0300

    First review

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from flask import jsonify, request, url_for
 
 from . import app, db
@@ -12,38 +14,34 @@ def add_short_url():
     site = url_for("index_view", _external=True)
     if not data:
         raise InvalidAPIUsage("Отсутствует тело запроса")
-
     if "url" not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!', 400)
-
+        raise InvalidAPIUsage(
+            '"url" является обязательным полем!', HTTPStatus.BAD_REQUEST
+        )
     custom_id = data.get("custom_id") or get_unique_short_id()
-
     if URLMap.query.filter_by(short=custom_id).first() is not None:
         raise InvalidAPIUsage(
             "Предложенный вариант короткой ссылки уже существует."
         )
-
     if not is_valid_custom_id(custom_id):
         raise InvalidAPIUsage(
-            "Указано недопустимое имя для короткой ссылки", 400
+            "Указано недопустимое имя для короткой ссылки",
+            HTTPStatus.BAD_REQUEST,
         )
-
     if len(custom_id) > 16:
         raise InvalidAPIUsage(
-            "Длина короткой ссылки не должна превышать 16 символов", 400
+            "Длина короткой ссылки не должна превышать 16 символов",
+            HTTPStatus.BAD_REQUEST,
         )
-
     short_url = URLMap(short=custom_id)
-    short_url.from_dict({"original": data["url"]})
-
+    short_url = URLMap(short=custom_id, original=data["url"])
     db.session.add(short_url)
     db.session.commit()
-
     return (
         jsonify(
             {"url": short_url.original, "short_link": site + short_url.short}
         ),
-        201,
+        HTTPStatus.CREATED,
     )
 
 
@@ -51,5 +49,5 @@ def add_short_url():
 def get_short_url(short_id):
     short_url = URLMap.query.filter_by(short=short_id).first()
     if short_url is None:
-        raise InvalidAPIUsage("Указанный id не найден", 404)
-    return jsonify({"url": short_url.original}), 200
+        raise InvalidAPIUsage("Указанный id не найден", HTTPStatus.NOT_FOUND)
+    return jsonify({"url": short_url.original}), HTTPStatus.OK
--- yacut/views.py
+++ yacut/views.py
@@ -9,32 +9,27 @@ from .utils import get_unique_short_id
 @app.route("/", methods=["GET", "POST"])
 def index_view():
     form = YacutForm()
-    if form.validate_on_submit():
-        original_url = form.original_link.data
-        site = url_for("index_view", _external=True)
-        short_id = form.custom_id.data or get_unique_short_id()
-        existing_short_id = URLMap.query.filter_by(short=short_id).first()
-        if existing_short_id:
-            flash("Предложенный вариант короткой ссылки уже существует.")
-        else:
-            url_map = URLMap(original=original_url, short=short_id)
-            db.session.add(url_map)
-            db.session.commit()
-            flash(
-                f"""Ваша новая ссылка готова: <a href="{site + short_id}">{site + short_id}</a>"""
-            )
-        return render_template(
-            "index.html",
-            original_url=original_url,
-            short_url=short_id,
-            form=form,
-        )
-
-    return render_template("index.html", form=form)
+    if not form.validate_on_submit():
+        return render_template("index.html", form=form)
+    original_url = form.original_link.data
+    site = url_for("index_view", _external=True)
+    short_id = form.custom_id.data or get_unique_short_id()
+    existing_short_id = URLMap.query.filter_by(short=short_id).first()
+    if existing_short_id is not None:
+        flash("Предложенный вариант короткой ссылки уже существует.")
+        return render_template("index.html", form=form)
+    url_map = URLMap(original=original_url, short=short_id)
+    db.session.add(url_map)
+    db.session.commit()
+    flash(
+        f"""Ваша новая ссылка готова: <a href="{site + short_id}">{site + short_id}</a>"""
+    )
+    return render_template(
+        "index.html", original_url=original_url, short_url=short_id, form=form
+    )
 
 
 @app.route("/<short_url>")
 def redirect_to_long(short_url):
     url_map = URLMap.query.filter_by(short=short_url).first_or_404()
-
     return redirect(url_map.original)
